6cffd5f12d5a28c632e209cb03662d8ae932610c
John Doe <faridazharizaidi123@gmail.com>
2023-12-31 09:55:45 +0800

    func

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,17 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+exports.scheduledFunction = functions.pubsub.schedule('0 9 * * *').timeZone('Asia/Singapore').onRun((context) => {
+ const currentTime = admin.firestore.Timestamp.now();
+ const cutoff = currentTime.seconds - (2 * 24 * 60 * 60); // 2 days ago
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+ return admin.firestore().collection('bookings')
+   .where('timestamp', '<', cutoff)
+   .get()
+   .then(snapshot => {
+     snapshot.docs.forEach(doc => {
+       doc.ref.delete();
+     });
+   });
+});
